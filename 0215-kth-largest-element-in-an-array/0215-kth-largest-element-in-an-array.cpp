82e1394d1468c98c0ca00436b57156862efaf874
Sushmigit <164911974+Sushmigit@users.noreply.github.com>
2024-07-11 13:34:48 +0530

    Time: 118 ms (10.73%), Space: 64.7 MB (11.35%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -5,7 +5,23 @@ public:
         sort(nums.begin(),nums.end(),greater<int>());
         return nums[k-1];
     }
+    int usingMaxHeap(vector<int> &nums,int k)
+    {
+        priority_queue<int>maxpq;
+        int i;
+        for(i=0;i<nums.size();i++)
+        {
+            maxpq.push(nums[i]);
+        }
+        while(k>1)
+        {
+            maxpq.pop();
+            k--;
+        }
+        return maxpq.top();
+    }
     int findKthLargest(vector<int>& nums, int k) {
-        return usingSort(nums,k);
+        //return usingSort(nums,k);//TC->O(NlogN) SC->O(1)
+        return usingMaxHeap(nums,k);
     }
 };
