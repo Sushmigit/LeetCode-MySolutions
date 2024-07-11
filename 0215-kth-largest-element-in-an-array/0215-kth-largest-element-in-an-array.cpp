8a88346fa4c5aa08d59efec3b5e8e32ce2c6f6c1
Sushmigit <164911974+Sushmigit@users.noreply.github.com>
2024-07-11 13:40:45 +0530

    Time: 84 ms (79.75%), Space: 61.6 MB (29.31%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -20,8 +20,27 @@ public:
         }
         return maxpq.top();
     }
+    int usingMinHeap(vector<int> &nums,int k)
+    {
+        priority_queue<int,vector<int>,greater<int>>minpq;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(minpq.size()<k)
+                minpq.push(nums[i]);
+            else
+            {
+                if(minpq.top()<nums[i])
+                {
+                    minpq.pop();
+                    minpq.push(nums[i]);
+                }
+            }
+        }
+        return minpq.top();
+    }
     int findKthLargest(vector<int>& nums, int k) {
         //return usingSort(nums,k);//TC->O(NlogN) SC->O(1)
-        return usingMaxHeap(nums,k);
+        //return usingMaxHeap(nums,k);//TC->O(N log N) SC->O(N)
+        return usingMinHeap(nums,k);
     }
 };
