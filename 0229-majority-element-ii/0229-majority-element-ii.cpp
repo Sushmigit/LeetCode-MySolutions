0fd7f04b5f7b304c0c464f6091a3cee65d525bf5
Sushmigit <164911974+Sushmigit@users.noreply.github.com>
2024-07-14 18:33:26 +0530

    Time: 14 ms (16.87%), Space: 18.6 MB (30.90%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -43,8 +43,49 @@ public:
        }
         return ans;
     }
+    vector<int> mooreVotingAlgo(vector<int>& nums)
+    {
+        int ele1 =-1,ele2 =-1,cnt1=0,cnt2=0;
+        int i;
+        int n = nums.size();
+        for(i=0;i<n;i++)
+        {
+            if(cnt1 == 0 && nums[i]!=ele2)
+            {
+                cnt1 = 1;
+                ele1 = nums[i];
+            }
+            else if(cnt2 == 0 && nums[i]!=ele1)
+            {
+                cnt2 = 1;
+                ele2 = nums[i];
+            }
+            else if(nums[i] == ele1)
+                cnt1++;
+            else if(nums[i] == ele2)
+                cnt2++;
+            else
+            {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        vector<int>ans;
+        cnt1 = 0;cnt2 = 0;
+        for(i=0;i<n;i++)
+        {
+            if(ele1 == nums[i]) cnt1++;
+            if(ele2 == nums[i]) cnt2++;
+        }
+        if(cnt1 > n/3)
+            ans.push_back(ele1);
+        if(cnt2 > n/3 && ele1!=ele2)
+            ans.push_back(ele2);
+        return ans;
+    }
     vector<int> majorityElement(vector<int>& nums) {
        //return naiveApproach(nums);//TC->O(N*N) SC->O(N)
-        return usingHash(nums);
+        //return usingHash(nums);//TC->O(N) SC->O(N)+O(N)
+        return mooreVotingAlgo(nums);
     }
 };
