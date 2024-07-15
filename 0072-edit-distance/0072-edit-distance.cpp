4030265d0e492e5cacdb8c2a7de8531e88de41cf
Sushmigit <164911974+Sushmigit@users.noreply.github.com>
2024-07-15 12:26:28 +0530

    Time: 13 ms (29.70%), Space: 13 MB (19.48%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -9,9 +9,30 @@ public:
         if(word1[i]==word2[j]) return dp[i][j]=minDis(i-1,j-1,word1,word2,dp);
         return dp[i][j]=1+min(minDis(i,j-1,word1,word2,dp),min(minDis(i-1,j,word1,word2,dp),minDis(i-1,j-1,word1,word2,dp)));
     }
+    int tabulation(int n,int m,string word1, string word2,vector<vector<int>>& dp)
+    {
+        int i,j;
+        //Base case
+        for(i=0;i<=n;i++) dp[i][0]=i;
+        for(j=0;j<=m;j++) dp[0][j]=j;
+        //Bottom-Up 
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=m;j++)
+            {
+                if(word1[i-1]==word2[j-1])
+                    dp[i][j]=dp[i-1][j-1];
+                else              
+                    dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+                                  //delete       //Insert    //Replace
+            }
+        }
+        return dp[n][m];
+    }
     int minDistance(string word1, string word2) {
         int n = word1.size(),m = word2.size();
-       vector<vector<int>>dp(n,vector<int>(m,-1));
-        return minDis(n-1,m-1,word1,word2,dp);//TC->O(N*M) SC->O(N*M)+O(N+M)
+       vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        //return minDis(n-1,m-1,word1,word2,dp);//TC->O(N*M) SC->O(N*M)+O(N+M)
+        return tabulation(n,m,word1,word2,dp);//TC->O(N*M) SC->O(N*M)
     }
 };
