01f3dfe28494ece3bdde53de9c1203f9e17573e9
Sushmigit <164911974+Sushmigit@users.noreply.github.com>
2024-07-16 00:50:14 +0530

    Time: 4 ms (38.23%), Space: 8.9 MB (6.27%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,7 +4,8 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         
         int n = matrix.size();
-         vector<vector<int>>ans(n,vector<int>(n,0));
+        //Naive Approach
+         /*vector<vector<int>>ans(n,vector<int>(n,0));
          int i,j;
          for(i=0;i<n;i++)//TC->O(N*M) SC->O(N*M)
          {
@@ -13,6 +14,23 @@ public:
                  ans[j][n-i-1] = matrix[i][j];
              }
          }
-      matrix = ans;
+      matrix = ans;*/
+        //Optimal Approach
+        //Transpose of the matrix
+        int i,j;
+        //O(n/2*n/2)->O(n^2)
+        for(i=1;i<n;i++)
+        {
+            for(j=0;j<i;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        //Reverse each row
+        //O(n^2)
+        for(i=0;i<n;i++)
+        {
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
     }
 };
