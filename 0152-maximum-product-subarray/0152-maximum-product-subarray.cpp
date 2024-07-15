19518605377a7f9dac2a90baf615eb6d0d493bd4
Sushmigit <164911974+Sushmigit@users.noreply.github.com>
2024-07-15 11:24:26 +0530

    Time: 7 ms (36.44%), Space: 16.7 MB (6.65%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int naive(vector<int>& nums)
+    {
+        int i,j,n=nums.size();
+        int maxi = INT_MIN;
+        for(i=0;i<n;i++)
+        {
+            int pro = 1;
+            for(j=i;j<n;j++)
+            {
+                pro*=nums[j];
+                maxi=max(maxi,pro);
+            }
+        }
+        return maxi;
+    }
+    int preSuf(vector<int>& nums)
+    {
+        long long pre=1,suf=1;
+        int i,n=nums.size();
+        long long maxi = LLONG_MIN;
+        for(i=0;i<n;i++)
+        {
+            if(pre==0) pre=1;
+            if(suf==0) suf=1;
+            if(pre<INT_MIN) pre=1;
+            if(suf<INT_MIN) suf=1;
+            pre = pre*nums[i];
+            suf = suf*nums[n-i-1];
+            maxi = max(maxi,max(pre,suf));
+        }
+        return maxi;
+    }
+    int maxProduct(vector<int>& nums) {
+        //return naive(nums);//TC->O(N*N) SC->O(1)
+        return preSuf(nums);//TC->O(N) SC->O(1)
+    }
+};
