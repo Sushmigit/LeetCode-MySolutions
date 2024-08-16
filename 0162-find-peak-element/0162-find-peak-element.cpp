e114ee958b02898f88a55ad299d14542dbbcc30d
Sushmigit <164911974+Sushmigit@users.noreply.github.com>
2024-08-16 18:12:57 +0530

    Time: 3 ms (64.94%), Space: 11.5 MB (50.04%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
+    int brute(vector<int>& nums)
+    {
         int n = nums.size();
         if( n == 1)
             return 0;
@@ -14,5 +15,32 @@ public:
         if(nums[n-1] > nums[n-2])
             return n-1;
        return 0;
+    }
+    int usingBN(vector<int>& nums)
+    {
+        int n = nums.size();
+        if(n == 1)
+            return 0;
+        if(nums[0] > nums[1])
+            return 0;
+        if(nums[n-1] >nums[n-2])
+            return n-1;
+        int l = 1,h=n-2,mid;
+        while(l<=h)
+        {
+            mid = (l+h)/2;
+           if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
+               return mid;
+           else if(nums[mid] < nums[mid-1])
+                 h = mid-1;
+            else 
+                l = mid+1;
+               
+        }
+        return -1;
+    }
+    int findPeakElement(vector<int>& nums) {
+        //return brute(nums);
+        return usingBN(nums);
     }
 };
