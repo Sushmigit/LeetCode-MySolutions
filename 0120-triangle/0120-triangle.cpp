3edd35ecdc63421dff421c6b686d51497af4bf81
Sushmigit <164911974+Sushmigit@users.noreply.github.com>
2024-08-26 21:55:15 +0530

    Time: 3 ms (83.45%), Space: 12.1 MB (7.18%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -9,12 +9,30 @@ public:
         int dg = triangle[i][j] + func(i+1,j+1,n,triangle,dp);
         return dp[i][j] = min(d,dg);
     }
+    int tabulation(vector<vector<int>>& triangle)
+    {
+         int n = triangle.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        for(int i=0;i<n;i++)
+            dp[n-1][i] = triangle[n-1][i];
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                int d = triangle[i][j] + func(i+1,j,n,triangle,dp);
+                int dg = triangle[i][j] + func(i+1,j+1,n,triangle,dp);
+                dp[i][j] = min(d,dg);
+            }
+        }
+        return dp[0][0];
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
         for(int i=0;i<n;i++)
             dp[n-1][i] = triangle[n-1][i];
-        func(0,0,n,triangle,dp);
-        return dp[0][0];
+        /*func(0,0,n,triangle,dp);
+        return dp[0][0];*/
+        return tabulation(triangle);
     }
 };
