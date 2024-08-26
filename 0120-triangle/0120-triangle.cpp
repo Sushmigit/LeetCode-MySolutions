c31d6cfe67ebb541d1a3f9b291e0e7aacfee319e
Sushmigit <164911974+Sushmigit@users.noreply.github.com>
2024-08-26 21:52:20 +0530

    Time: 4 ms (73.70%), Space: 11.8 MB (7.18%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int func(int i,int j,int n,vector<vector<int>>& triangle,vector<vector<int>>& dp)
+    {
+        if(i == n-1)
+            return triangle[n-1][j];
+        if(dp[i][j]!=-1) return dp[i][j];
+        int d = triangle[i][j] + func(i+1,j,n,triangle,dp);
+        int dg = triangle[i][j] + func(i+1,j+1,n,triangle,dp);
+        return dp[i][j] = min(d,dg);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        for(int i=0;i<n;i++)
+            dp[n-1][i] = triangle[n-1][i];
+        func(0,0,n,triangle,dp);
+        return dp[0][0];
+    }
+};
