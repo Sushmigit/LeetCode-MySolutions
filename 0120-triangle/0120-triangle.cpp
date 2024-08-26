676e7b76bed16b206900acd0264e085924a4c8f6
Sushmigit <164911974+Sushmigit@users.noreply.github.com>
2024-08-26 21:58:12 +0530

    Time: 9 ms (10.50%), Space: 12.5 MB (7.18%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -13,18 +13,21 @@ public:
     {
          int n = triangle.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
+        vector<int>front(n,0);
         for(int i=0;i<n;i++)
-            dp[n-1][i] = triangle[n-1][i];
+            front[i] = triangle[n-1][i];
         for(int i=n-2;i>=0;i--)
         {
+            vector<int>temp(n,0);
             for(int j=i;j>=0;j--)
             {
-                int d = triangle[i][j] + func(i+1,j,n,triangle,dp);
-                int dg = triangle[i][j] + func(i+1,j+1,n,triangle,dp);
-                dp[i][j] = min(d,dg);
+                int d = triangle[i][j] + front[j];
+                int dg = triangle[i][j] + front[j+1];
+                temp[j] = min(d,dg);
             }
+            front = temp;
         }
-        return dp[0][0];
+        return front[0];
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
