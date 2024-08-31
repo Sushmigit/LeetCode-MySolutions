3ccb0a1f724c9749a0d5e3dca6e530529487005d
Sushmigit <164911974+Sushmigit@users.noreply.github.com>
2024-08-31 17:16:44 +0530

    Time: 3 ms (96.27%), Space: 21.1 MB (86.44%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,12 +1,42 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& arr) {
-      set<int>st(arr.begin(),arr.end());
+    int brute(vector<int>& arr)
+    {
+        set<int>st(arr.begin(),arr.end());
         int ind = 0;
         for(auto it : st)
         {
             arr[ind++] = it;
         }
         return ind;
+    }
+    int twoPointer(vector<int>& arr)
+    {
+        /*int i = 0,j;
+        int n = arr.size();
+       for(int j=i+1;j<n;j++)
+       {
+           if(arr[j]!=arr[i])
+           {
+               i++;
+               arr[i] = arr[j];
+           }
+       }
+        return i;*/
+        int i = 1;
+int n = arr.size();
+for(int j = 1; j < n; j++)
+{
+    if(arr[j] != arr[j-1])
+    {
+       
+        arr[i++] = arr[j];
+    }
+}
+        return i;
+    }
+    int removeDuplicates(vector<int>& arr) {
+        //return brute(arr);
+          return twoPointer(arr);
     }
 };
