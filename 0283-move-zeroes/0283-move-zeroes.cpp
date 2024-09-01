eef9863adc53c0c755f86d22e4a68d3663421521
Sushmigit <164911974+Sushmigit@users.noreply.github.com>
2024-09-01 09:37:53 +0530

    Time: 15 ms (67.40%), Space: 21.9 MB (66.53%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-        int cnt = 0,n = nums.size();
+    void brute(vector<int>& nums)
+    {
+         int cnt = 0,n = nums.size();
         vector<int>temp(n,0);
         int ind = 0;
        for(int i=0;i<n;i++)
@@ -17,5 +18,29 @@ public:
             temp.push_back(0);*/
         for(int i=0;i<n;i++)
             nums[i] = temp[i];
+    }
+    void moveZeroes(vector<int>& nums) {
+        int cnt = 0,n = nums.size();
+        int i,j;
+        //brute(nums);
+        for(int k = 0;k<n;k++)
+        {
+            if(nums[k]==0)
+            {
+                i = k;
+                break;
+            }
+        }
+        j = i+1;
+        while(j<n && i<n)
+        {
+            if(nums[j]!=0)
+            {
+                swap(nums[i],nums[j]);
+                i++;
+                
+            }
+            j++;
+        }
     } 
 };
