1862aa14bf3682d4846922f3dab2322d943b4147
Sushmigit <164911974+Sushmigit@users.noreply.github.com>
2024-09-21 14:02:32 +0530

    Time: 756 ms (21.35%), Space: 0B (100.00%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,2 @@
+/* Write your PL/SQL query statement below */
+select w1.id from weather w1 join weather w2 on w1.recorddate = w2.recorddate + interval '1' day where w1.temperature > w2.temperature;
